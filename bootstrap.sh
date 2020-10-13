510b0f1ed354d11338cb1c211e54616c1783f1ff
ajaycheenath@gmail.com <ajaycheenath@gmail.com>
2020-10-13 12:56:49 +0200

    Changed mysql version

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 echo "Initializing infra components"
 
-MYSQL_PORT=50000
+MYSQL_PORT=50001
 MYSQL_PASSWORD=password
+MYSQL_HOST=127.0.0.1
 #Start mysql docker instance
 docker stop infra-mysql
 docker rm infra-mysql
-docker run --detach --name=infra-mysql -p ${MYSQL_IP}:${MYSQL_PORT}:3306  --env="MYSQL_ROOT_PASSWORD=${MYSQL_PASSWORD}" mysql:8.0.21
+docker run  --detach --name=infra-mysql -p ${MYSQL_HOST}:${MYSQL_PORT}:3306  --env="MYSQL_ROOT_PASSWORD=${MYSQL_PASSWORD}" mysql:8.0
+sleep 10
 
 echo "Mysql running on port ${MYSQL_PORT}"
-echo "To connect use: mysql -uroot -p${MYSQL_PASSWORD}  --host=0.0.0.0 --port=50000"
+echo "To connect use: mysql -uroot -p${MYSQL_PASSWORD}  --host=${MYSQL_HOST} --port=${MYSQL_PORT}"
