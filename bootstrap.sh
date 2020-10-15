9d58a0752a56db19ff52ac28ec8a4224746d4fd6
ajaycheenath@gmail.com <ajaycheenath@gmail.com>
2020-10-15 16:25:21 +0200

    Create stage db script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 echo "Initializing infra components"
 
-MYSQL_PORT=50001
+MYSQL_PORT=3306
 MYSQL_PASSWORD=password
 MYSQL_HOST=127.0.0.1
 #Start mysql docker instance
-docker stop infra-mysql
-docker rm infra-mysql
-docker run  --detach --name=infra-mysql -p ${MYSQL_HOST}:${MYSQL_PORT}:3306  --env="MYSQL_ROOT_PASSWORD=${MYSQL_PASSWORD}" mysql:8.0
-sleep 10
+docker stop mysql
+docker rm mysql
+docker run  --detach --name=mysql -p ${MYSQL_HOST}:${MYSQL_PORT}:3306  --env="MYSQL_ROOT_PASSWORD=${MYSQL_PASSWORD}" mysql:8.0
+sleep 20
+
+
 
 echo "Mysql running on port ${MYSQL_PORT}"
 echo "To connect use: mysql -uroot -p${MYSQL_PASSWORD}  --host=${MYSQL_HOST} --port=${MYSQL_PORT}"
---
+++ createStageDb.sh
@@ -0,0 +1,2 @@
+mysql -uroot -ppassword  --host=127.0.0.1 --port=3306 -e "drop database stagedb;"
+mysql -uroot -ppassword  --host=127.0.0.1 --port=3306 -e "create database stagedb;"
